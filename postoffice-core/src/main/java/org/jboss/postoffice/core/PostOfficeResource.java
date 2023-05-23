9624a82f5d1df9b432cb3abb7081cd339e561d38
R Searls <rsearls@redhat.com>
2023-05-23 15:13:50 -0400

    added delete and checkmail to PostOffice and updata docs

--- postoffice-core/src/main/java/org/jboss/postoffice/core/PostOfficeResource.java
+++ postoffice-core/src/main/java/org/jboss/postoffice/core/PostOfficeResource.java
@@ -10,9 +10,11 @@ import java.util.Map;
 import jakarta.servlet.ServletContext;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
-import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.MultivaluedMap;
@@ -26,8 +28,9 @@ import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
  */
 @Path("/postoffice")
 public class PostOfficeResource {
-    // todo by location by config
-    public static final String UPLOAD_FOLDER = "/tmp/z";
+
+    private static String env_mailbox =  System.getenv("MAILBOX");
+    private static final String MAILBOX_FOLDER = (env_mailbox==null ? "/tmp/mailbox" : env_mailbox);
 
     @Context
     private ServletContext context;
@@ -35,46 +38,83 @@ public class PostOfficeResource {
     @GET
     @Path("/ping")
     @Produces("text/plain")
-    // debug
     public String ping() {
-        return "postoffice-pong";
+        return "pong \n";
     }
 
-    @PUT
+    @GET
+    @Path("/checkmail")
+    @Produces("text/plain")
+    public String checkmail() {
+        String report;
+        File mailbox = new File(MAILBOX_FOLDER);
+        if (mailbox.exists()) {
+            String [] mList = mailbox.list();
+            if (mList != null && mList.length > 0) {
+                StringBuilder sb = new StringBuilder();
+                sb.append("mailbox file cnt: " + mList.length + "\n");
+                for (int i=0; i < mList.length; i++ ) {
+                    sb.append(mList[i] + "\n");
+                }
+                report = MAILBOX_FOLDER + ": " + sb.toString();
+            } else {
+                report = MAILBOX_FOLDER + " is empty";
+            }
+
+        } else {
+            report = "directory " + MAILBOX_FOLDER + " does not exist";
+        }
+        return report + "\n";
+    }
+
+    @POST
     @Path("/upload")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     public Response uploadFile(MultipartFormDataInput input) throws IOException {
-
         Map<String, List<InputPart>> uploadForm = input.getFormDataMap();
 
         // Get file data to save
         List<InputPart> inputParts = uploadForm.get("attachment");
-
         for (InputPart inputPart : inputParts) {
             try {
                 MultivaluedMap<String, String> header = inputPart.getHeaders();
                 String fileName = getFileName(header);
-
                 // convert the uploaded file to inputstream
                 InputStream inputStream = inputPart.getBody(InputStream.class, null);
-                File customDir = new File(UPLOAD_FOLDER);
-                if (!customDir.exists()) {
-                    customDir.mkdir();
+                File mailbox = new File(MAILBOX_FOLDER);
+                if (!mailbox.exists()) {
+                    mailbox.mkdir();
                 }
-                fileName = customDir.getCanonicalPath() + File.separator + fileName;
+                fileName = mailbox.getCanonicalPath() + File.separator + fileName;
 
                 try (FileOutputStream outputStream = new FileOutputStream(fileName)) {
                     inputStream.transferTo(outputStream);
                 }
 
                 return Response.status(200).entity("Uploaded file name : " + fileName
-                        + " . <br/> <a href='" + context.getContextPath() + "'>Back</a>").build();
+                        + "\n").build();
 
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-        return null;
+        return Response.status(200).build();
+    }
+
+    @DELETE
+    @Path("/delete/{filename}")
+    public Response deleteFile(@PathParam("filename") String filename) throws Exception {
+        File mailbox = new File(MAILBOX_FOLDER);
+
+        File f = new File(mailbox.getCanonicalPath() + File.separator + filename);
+        if (f.exists() && f.isFile()) {
+            f.delete();
+        } else if (!f.exists()){
+            return Response.status(400).entity(filename + " does not exist\n").build();
+        } else if (f.isDirectory()) {
+            return Response.status(400).entity(filename + " is a directory not a file\n").build();
+        }
+        return Response.status(200).entity("success\n").build();
     }
 
     private String getFileName(MultivaluedMap<String, String> header) {
